eaf3cf7678cf3c3458fbe878a5ce8da61187d800
Oleg Pyatko <olegpyatko@Olegs-iMac.local>
2017-04-17 11:07:42 +0300

    complete

---
+++ Lab2/Lab2/GameTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  GameTableViewCell.swift
+//  Lab2
+//
+//  Created by Oleg Pyatko on 3/21/17.
+//  Copyright © 2017 Oleg Pyatko. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+    
+    @IBOutlet var gameNameLabel: UILabel!
+    
+    @IBOutlet var previewImage: UIImageView!
+    @IBOutlet var shortDescriptionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
